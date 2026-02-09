1d3780861fceb5d4f92a2b350d23c38fc73b4b80
Shafin Chowdhury <shafinchowdhury2001@gmail.com>
2026-02-09 23:27:56 +0600

    fixed Loading

--- src/components/dashboard/revenueChart.tsx
+++ src/components/dashboard/revenueChart.tsx
@@ -22,7 +22,7 @@ const { dateRange, isLoading } = useDashboardStore();
     return fullData;
   };
 
-  if (isInitialLoading) {
+  if (isLoading) {
     return (
       <div className="h-[300px] w-full bg-gray-50 animate-pulse rounded-lg flex items-center justify-center">
         <div className="w-full h-1/2 border-b border-gray-200 flex items-end justify-around px-4">
